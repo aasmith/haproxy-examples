2c4e2e5e967a3ee8400775db34113a39460a1dc8
Andrew A Smith <andy@tinnedfruit.org>
2016-03-06 22:11:03 -0800

    Use canhazip.com for more readable output

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ docker run -d \
   --net=host --pid=host \
   --ulimit nofile=1000000 \
   -e APP_NAME="appname-example $MADE_UP_BUILD_NUMBER" \
-  -e SERVER=www.google.com:80 \
+  -e SERVER=ipv4.icanhazip.com:80 \
   --name appname-example-$MADE_UP_BUILD_NUMBER \
   haproxy-example
 
